469ae5165c7148ef1e1421706bed60f87e051806
sardylan <sardylan@gmail.com>
2020-08-13 19:39:31 +0200

    Adds Bootstrap in Vue

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,11 @@
 import Vue from "vue";
 import App from "./App.vue";
+import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
+import websocketService from "@/services/websocket.service";
 
 Vue.config.productionTip = false;
+Vue.use(BootstrapVue);
+Vue.use(IconsPlugin);
 
 new Vue({
   render: h => h(App)
